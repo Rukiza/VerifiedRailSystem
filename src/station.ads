f4f8156ba8c939f3804a954a894906c92fdc3965
Shane Brewer <theproros@gmail.com>
2016-08-28 17:44:07 +1200

    Station has a diffinition of Init.

--- src/station.ads
+++ src/station.ads
@@ -10,12 +10,14 @@ is
      (Amount_Incomming: Positive;
       Amount_Outgoing: Positive) is
       record
-         Incomming: Track.Tracks_Array (1 .. Amount_Incomming);
-         Outgoing: Track.Tracks_Array (1 .. Amount_Outgoing);
+         Incomming: Tracks_Array (1 .. Amount_Incomming);
+         Outgoing: Tracks_Array (1 .. Amount_Outgoing);
       end record;
 
 begin
 
+   function Init_Station_Record (In: Track_Array;
+                                 Out: Track_Array) return Station_Record;
 
 
 end Station;
