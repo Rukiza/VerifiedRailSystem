78efa1665d4d2f15036b5c78006bde85f30dc30c
Shane Brewer <theproros@gmail.com>
2016-09-10 16:57:39 +1200

    Created init methods in the body files.

--- src/station.adb
+++ src/station.adb
@@ -1,5 +1,9 @@
+with Id_Handler; use Id_Handler;
+
 package body Station
 with SPARK_Mode => On
 is
+   function Init_Station_Record return Station_Record is
+     ((Station_ID => Get_ID));
 
 end Station;
--- src/track.adb
+++ src/track.adb
@@ -1,7 +1,12 @@
+with Id_Handler; use Id_Handler;
+
 package body Track
-with SPARK_Mode => On
 is
 
-
+function Init_Track_Record (O: in Station_Record;
+                            D: in Station_Record) return Track_Record is
+      ((Track_ID => Get_ID,
+        Origin => O,
+        Destination => D));
 
 end Track;
